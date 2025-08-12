04d965ba0f34633b6bb813bbc8b897d19f3354bc
EazzyHacks <yhair7200@gmail.com>
2025-08-12 18:28:36 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+
+### `𝐀𝐍𝐆𝐄𝐋 𝐁𝐎𝐓 🧨`
+![Menu Principa](https://files.catbox.moe/ntyp5r.jpg)
+</p>
---
+++ config.js
@@ -0,0 +1,72 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = [
+  ['51936994155', 'jid', 'Ｇｘ   Ｓｔｏｒｅ   Ｖｉｐ', true],
+  ['222265178386528@lid', 'lid', '@evolution.hack', true],
+  ['51936994155', 'jid', 'Ｇｘ   Ｓｔｏｒｅ   Ｖｉｐ', true],
+  [''],
+  [''],
+  [''],
+  ['']
+]
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = []
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = `Ｇｘ   Ｓｔｏｒｅ   Ｖｉｐ`
+global.author = 'Ｇｘ   Ｓｔｏｒｅ   Ｖｉｐ'
+global.wait = 'Ｇｘ   Ｓｔｏｒｅ   Ｖｉｐ'
+global.botname = 'Ｇｘ   Ｓｔｏｒｅ   Ｖｉｐ'
+global.textbot = `Ｇｘ   Ｓｔｏｒｅ   Ｖｉｐ`
+global.listo = 'Ｇｘ   Ｓｔｏｒｅ   Ｖｉｐ'
+global.namechannel = 'Ｇｘ   Ｓｔｏｒｅ   Ｖｉｐ'
+global.baileys = '@whiskeysockets/baileys'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.catalogo = 'https://files.catbox.moe/l0saph.jpg'
+global.miniurl = 'https://files.catbox.moe/l0saph.jpg'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.group = 'https://chat.whatsapp.com/I4yJ2vrlhGXH3JRg06mxFZ?mode=ac_t'
+global.canal = 'https://whatsapp.com/channel/0029Vb5oUp43LdQUVViHwc0m'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: botname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+global.moment = moment        
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.multiplier = 69 
+global.maxwarn = '2' // máxima advertencias
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
